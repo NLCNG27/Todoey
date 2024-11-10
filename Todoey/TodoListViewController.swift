a084abbdcba7b18f05e47ec96d278a0ea224e47e
Nguyen Lam <nguyen@cngsoftware.com>
2024-11-09 18:18:13 -0600

    added datasource methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Eating", "Coding", "Working Out"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+    }
+    
+    
+    //MARK: - Tableview Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a reusable cell with the identifier "ToDoItemCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        
+        // Set the text label of the cell to the corresponding item in the itemArray
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
